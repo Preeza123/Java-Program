8987a2f341ab8345fc578925c6d51c77516d37f9
Preeza123 <139269025+Preeza123@users.noreply.github.com>
2025-02-05 11:43:24 +0530

    Add files via upload

---
+++ NumberComparison.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class NumberComparison {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // Taking input for two numbers
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        
+        // Using relational operators to compare numbers
+        System.out.println(num1 + " < " + num2 + " is " + (num1 < num2));
+        System.out.println(num1 + " <= " + num2 + " is " + (num1 <= num2));
+        System.out.println(num1 + " > " + num2 + " is " + (num1 > num2));
+        System.out.println(num1 + " >= " + num2 + " is " + (num1 >= num2));
+        System.out.println(num1 + " == " + num2 + " is " + (num1 == num2));
+        System.out.println(num1 + " != " + num2 + " is " + (num1 != num2));
+        
+        sc.close();
+    }
+}
